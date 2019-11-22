2495f782c5cc26568a1613fd720bcee25bf15a0a
Nimisha Wilson <nimishawilson123@gmail.com>
2019-11-22 22:29:42 +0530

    arrays

--- object-and-arrays-reference-vs-copy/index.js
+++ object-and-arrays-reference-vs-copy/index.js
@@ -10,3 +10,32 @@ let name2 = name;
 console.log(name,name2);
 name = 'lisi';
 console.log(name,name2);
+
+
+//array
+console.log('--------------------');
+const players = ['nisha','annu','lessly','bony'];
+const team = players;
+console.log(players,team);
+team[3]='lux';
+console.log(players,team);
+
+//creating copy
+console.log('--------------------');
+const team2 = players.slice(); //slice takes a copy of orginal array
+console.log(team2)
+
+//create new array and concatinate old one
+const team3 = [].concat(players); 
+console.log(team2)
+
+//use es6 spread
+const team4 = [...players];
+team4[3] = 'heeee';
+console.log(team4);
+
+const team5 = Array.from(players);
+team5[3]='cool';
+console.log(team5);
+console.log(players);
+
